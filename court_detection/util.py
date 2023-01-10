1be33e28af91a0456870ba831e2139f95cb07545
oroshi <oroshinekko@gmail.com>
2023-01-10 13:02:43 +0900

    add comment

--- court_detection/util.py
+++ court_detection/util.py
@@ -5,14 +5,14 @@ FACTOR = 1
 
 def _to_ml(x: float, img_x: int) -> float:
     """
-    0 <= x <= img_x -> -1 <= x <= 1
+    0 <= x <= img_x -> -0.5 <= x <= 0.5
     """
     return ((x / img_x) - 0.5) * FACTOR
 
 
 def _to_img(x: float, img_x: int) -> float:
     """
-    -1 <= x <= 1 -> 0 <= x <= img_x
+    -0.5 <= x <= 0.5 -> 0 <= x <= img_x
     """
     return ((x / FACTOR) + 0.5) * img_x
 
